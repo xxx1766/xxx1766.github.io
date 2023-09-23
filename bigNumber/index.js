176142a0d41c159487ec2ee89f0f5a0255f0092d
xxx1766 <xuqingru19@mails.ucas.ac.cn>
2023-09-23 14:23:24 +0800

    Site updated: 2023-09-23 14:23:24

--- bigNumber/index.js
+++ bigNumber/index.js
@@ -49,223 +49,12 @@
     splitNum: '',
     strNum:  ''
   }
-  document.getElementById("numArea").innerHTML = isNumber?(isPart ? coreContext.splitNum : coreContext.randNum):'';
-  document.getElementById("numArea").onclick = function () {
-    changeNumShow();
-    document.getElementById("numArea").innerHTML = isNumber?(isPart ? coreContext.splitNum : coreContext.randNum):'';
-  };
-  document.getElementById("engArea").innerHTML = isEnglish?coreContext.strNum:'';
-  // 获取训练等级
-  function myLevel() {
-    var level = parseInt(document.getElementById("level").value);
-    console.log(level);
-    //把字符串转换成数字
-    return level;
-  }
-  // 切换数字显示形式
-  function changeNumShow() {
-    isPart = !isPart;
-  }
-  function initContext() {
-    coreContext.randNum = '';
-    coreContext.splitNum = '';
-    coreContext.strNum = '';
-  }
-  // 生成数字
-  function createNum() {
-    let dim = myLevel();
-    if(dim == 0){
-      dim = Math.pow(10, Math.floor(Math.random()*10)+8);
-    }
-    return String(Math.floor(Math.random()*dim)/100);
-  }
-  // 数字=>辅助阅读
-  function num2Part(num) {
-    let sliceNum =  Array.from(num)
-    let newFormalNum = []
-    let count = 0
-    let flagDot = false
-    for (let i = 0; i < sliceNum.length; i++) {
-      newFormalNum.push(sliceNum[sliceNum.length - i - 1]);
-      
-      if (flagDot) {
-        count++
-        if (count === 3 && i !== sliceNum.length - 1) {
-          newFormalNum.push(',')
-          count = 0;
-        }
-      }
-      if (sliceNum[sliceNum.length - i - 1] === '.') {
-        flagDot = true;
-      }
-    }
-    return newFormalNum.reverse().join('');
-  }
-  // 数字=>英文
-  function num2En(stringNum) {
-    let intNum = stringNum.split('.')[0];
-    let dotNum = stringNum.split('.')[1];
-    let lenNum = intNum.length;
-    let res = '';
-    while (lenNum > 0) {
-      if (lenNum >= 16) {
-        res += "it's too big!!!";
-        break;
-      } else if (lenNum >=13) {
-        let substr = intNum.slice(0, -12);
-        res += partNum(substr) + ' ' + unitNum[1000000000000] + ' ';
-        intNum = intNum.slice(-12);
-        lenNum = 12;
-      } else if (lenNum >= 10) {
-        let substr = intNum.slice(0, -9);
-        res += partNum(substr) + ' ' + unitNum[1000000000] + ' ';
-        intNum = intNum.slice(-9);
-        lenNum = 9;
-      } else if (lenNum >= 7) {
-        let substr = intNum.slice(0, -6);
-        res += partNum(substr) + ' ' + unitNum[1000000] + ' ';
-        intNum = intNum.slice(-6);
-        lenNum = 6;
-      } else if (lenNum >= 4) {
-        let substr = intNum.slice(0, -3);
-        res += partNum(substr) + ' ' + unitNum[1000] + ' ';
-        intNum = intNum.slice(-3);
-        lenNum = 3;
-      } else {
-        let substr = intNum;
-        res += partNum(substr);
-        lenNum = 0;
-      }
-    }
-    if (dotNum!='' || dotNum!='.' || dotNum!=undefined) {
-      res += ' dot ';
-      for (let i = 0; i < dotNum.length; i++) {
-        res += oneNum(dotNum[i]);
-        if (i != dotNum-1) {
-          res += ' ';
-        }
-      }
-    }
-    return res;
-  }
-  function partNum(substr) {
-    let lenNum = substr.length;
-    let res = '';
   
-    if (lenNum === 1) {
-      res = oneNum(substr);
-    } else if (lenNum === 2) {
-      if (substr[0] === '0') {
-        res = oneNum(substr[1]);
-      } else {
-        res = twoNum(substr);
-      }
-    } else if (lenNum === 3) {
-      if (substr[0] === '0' && substr[1] === '0' && substr[2] === '0') {
-        res = oneNum('0');
-      } else if (substr[0] === '0' && substr[1] === '0') {
-        res = oneNum(substr[2]);
-      } else if (substr[0] === '0') {
-        res = twoNum(substr.slice(1));
-      } else {
-        res = threeNum(substr);
-      }
-    }
-    return res;
-  }
-  function oneNum(substr) {
-    return unitNum[parseInt(substr)];
-  }
-  function twoNum(substr) {
-    if (substr[0] === '0') {
-      return oneNum(substr[1]);
-    } else if (substr[0] === '1') {
-      return unitNum[10 + parseInt(substr[1])];
-    } else {
-      if (substr[1] === '0') {
-        return unitNum[parseInt(substr[0]) * 10];
-      } else {
-        return unitNum[parseInt(substr[0]) * 10] + ' ' + oneNum(substr[1]);
-      }
-    }
-  }
-  function threeNum(substr) {
-    if (substr[2] === '0' && substr[1] === '0') {
-      return unitNum[parseInt(substr[0])] + ' ' + unitNum[100]
-    } else {
-      return unitNum[parseInt(substr[0])] + ' ' + unitNum[100] + ' and ' + twoNum(substr.slice(1));
-    }
-  }
-  function screenclear() {
-    isEnglish = false;
-    isNumber = false;
-    document.getElementById("numArea").innerHTML = '';
-    document.getElementById("engArea").innerHTML = '';
-  }
-
-  var prompt = function(message, style, time) {
-    style = (style === undefined) ? 'alert-success' : style;
-    time = (time === undefined) ? 1200 : time;
-    $('<div id="promptModal">')
-        .appendTo('body')
-        .addClass('alert ' + style)
-        .css({
-            "display": "block",
-            "z-index": 99999,
-            "left": ($(document.body).outerWidth(true) - 120) / 2,
-            "top": ($(window).height() - 45) / 2,
-            "position": "absolute",
-            "padding": "20px",
-            "border-radius": "5px",
-            "color": "#fff",
-            "background-color": "#009900",
-            "opacity": 0.6,
-            "font-size": "20px",
-            "text-align": "center"
-        })
-        .html(message)
-        .show()
-        .delay(time)
-        .fadeOut(10, function () {
-            $('#promptModal').remove();
-        });
-  };
-
-  document.getElementById("createNumber").onclick = function () {
-    initContext();
-    screenclear();
-    coreContext.randNum = createNum();
-    coreContext.splitNum = num2Part(coreContext.randNum);
-    coreContext.strNum = num2En(coreContext.randNum);
-    // window.alert('生成成功');
-    changeBtnStatus("UNDEFINED");
-    ttsWS?.close();
-    audioPlayer.reset();
-    prompt('生成成功', 'alert-success', 500);
-  }
-  
-  document.getElementById("clear").onclick = function () {
-    screenclear();
-    changeBtnStatus("STOP");
-    changeBtnStatus("UNDEFINED");
-    prompt('清空成功', 'alert-success', 500);
-  }
-
-  document.getElementById("help").onclick = function () {
-    confirm("帮助:\n1. 点击生成数字，生成一个随机的数字，!!点击数字区域内的数字可以切换显示形式辅助读数!!。\n2. 点击语音，可以进行语音播放。\n3. 点击清屏，可以清空数字和英文。\n4. 在左侧第三栏可调节发音参数。\n");
-  }
-  
-  document.getElementById("showNumber").onclick = function () {
-    isNumber = true;
-    document.getElementById("numArea").innerHTML = isNumber?(isPart ? coreContext.splitNum : coreContext.randNum):'';
-  }
-  document.getElementById("showEnglish").onclick = function () {
-    isEnglish = true;
-    document.getElementById("engArea").innerHTML = isEnglish?coreContext.strNum:'';
-  }
-
+  const audioPlayer = new AudioPlayer("../../dist");
   let btnControl = document.getElementById("controll_tts");
   let btnStatus = "UNDEFINED"; // "UNDEFINED" "CONNECTING" "PLAY" "STOP"
+  let ttsWS;
+
   // 控制播放键的显示状态
   function changeBtnStatus(status) {
     btnStatus = status;
@@ -279,14 +68,7 @@
       btnControl.innerText = "重新播放";
     }
   }
-  const audioPlayer = new AudioPlayer("../../dist");
-  audioPlayer.onPlay = () => {
-    changeBtnStatus("PLAY");
-  };
-  audioPlayer.onStop = (audioDatas) => {
-    console.log(audioDatas);
-    btnStatus === "PLAY" && changeBtnStatus("STOP");
-  };
+
   function getWebSocketUrl(apiKey, apiSecret) {
     var url = "wss://tts-api.xfyun.cn/v2/tts";
     var host = location.host;
@@ -301,6 +83,7 @@
     url = `${url}?authorization=${authorization}&date=${date}&host=${host}`;
     return url;
   }
+
   function encodeText(text, type) {
     if (type === "unicode") {
       let buf = new ArrayBuffer(text.length * 4);
@@ -320,7 +103,6 @@
     }
   }
 
-  let ttsWS;
   function connectWebSocket() {
     const url = getWebSocketUrl(API_KEY, API_SECRET);
     console.log(url)
@@ -389,6 +171,241 @@
       // console.log(e);
     };
   }
+
+  // 获取训练等级
+  function myLevel() {
+    var level = parseInt(document.getElementById("level").value);
+    console.log(level);
+    //把字符串转换成数字
+    return level;
+  }
+
+  // 切换数字显示形式
+  function changeNumShow() {
+    isPart = !isPart;
+  }
+  function initContext() {
+    coreContext.randNum = '';
+    coreContext.splitNum = '';
+    coreContext.strNum = '';
+  }
+
+  // 生成数字
+  function createNum() {
+    let dim = myLevel();
+    if(dim == 0){
+      dim = Math.pow(10, Math.floor(Math.random()*10)+8);
+    }
+    return String(Math.floor(Math.random()*dim)/100);
+  }
+
+  // 数字=>辅助阅读
+  function num2Part(num) {
+    let sliceNum =  Array.from(num)
+    let newFormalNum = []
+    let count = 0
+    let flagDot = false
+    for (let i = 0; i < sliceNum.length; i++) {
+      newFormalNum.push(sliceNum[sliceNum.length - i - 1]);
+      
+      if (flagDot) {
+        count++
+        if (count === 3 && i !== sliceNum.length - 1) {
+          newFormalNum.push(',')
+          count = 0;
+        }
+      }
+      if (sliceNum[sliceNum.length - i - 1] === '.') {
+        flagDot = true;
+      }
+    }
+    return newFormalNum.reverse().join('');
+  }
+
+  // 数字=>英文
+  function num2En(stringNum) {
+    let intNum = stringNum.split('.')[0];
+    let dotNum = stringNum.split('.')[1];
+    let lenNum = intNum.length;
+    let res = '';
+    while (lenNum > 0) {
+      if (lenNum >= 16) {
+        res += "it's too big!!!";
+        break;
+      } else if (lenNum >=13) {
+        let substr = intNum.slice(0, -12);
+        res += partNum(substr) + ' ' + unitNum[1000000000000] + ' ';
+        intNum = intNum.slice(-12);
+        lenNum = 12;
+      } else if (lenNum >= 10) {
+        let substr = intNum.slice(0, -9);
+        res += partNum(substr) + ' ' + unitNum[1000000000] + ' ';
+        intNum = intNum.slice(-9);
+        lenNum = 9;
+      } else if (lenNum >= 7) {
+        let substr = intNum.slice(0, -6);
+        res += partNum(substr) + ' ' + unitNum[1000000] + ' ';
+        intNum = intNum.slice(-6);
+        lenNum = 6;
+      } else if (lenNum >= 4) {
+        let substr = intNum.slice(0, -3);
+        res += partNum(substr) + ' ' + unitNum[1000] + ' ';
+        intNum = intNum.slice(-3);
+        lenNum = 3;
+      } else {
+        let substr = intNum;
+        res += partNum(substr);
+        lenNum = 0;
+      }
+    }
+    if (dotNum!='' || dotNum!='.' || dotNum!=undefined) {
+      res += ' dot ';
+      for (let i = 0; i < dotNum.length; i++) {
+        res += oneNum(dotNum[i]);
+        if (i != dotNum-1) {
+          res += ' ';
+        }
+      }
+    }
+    return res;
+  }
+
+  function partNum(substr) {
+    let lenNum = substr.length;
+    let res = '';
+  
+    if (lenNum === 1) {
+      res = oneNum(substr);
+    } else if (lenNum === 2) {
+      if (substr[0] === '0') {
+        res = oneNum(substr[1]);
+      } else {
+        res = twoNum(substr);
+      }
+    } else if (lenNum === 3) {
+      if (substr[0] === '0' && substr[1] === '0' && substr[2] === '0') {
+        res = oneNum('0');
+      } else if (substr[0] === '0' && substr[1] === '0') {
+        res = oneNum(substr[2]);
+      } else if (substr[0] === '0') {
+        res = twoNum(substr.slice(1));
+      } else {
+        res = threeNum(substr);
+      }
+    }
+    return res;
+  }
+
+  function oneNum(substr) {
+    return unitNum[parseInt(substr)];
+  }
+
+  function twoNum(substr) {
+    if (substr[0] === '0') {
+      return oneNum(substr[1]);
+    } else if (substr[0] === '1') {
+      return unitNum[10 + parseInt(substr[1])];
+    } else {
+      if (substr[1] === '0') {
+        return unitNum[parseInt(substr[0]) * 10];
+      } else {
+        return unitNum[parseInt(substr[0]) * 10] + ' ' + oneNum(substr[1]);
+      }
+    }
+  }
+
+  function threeNum(substr) {
+    if (substr[2] === '0' && substr[1] === '0') {
+      return unitNum[parseInt(substr[0])] + ' ' + unitNum[100]
+    } else {
+      return unitNum[parseInt(substr[0])] + ' ' + unitNum[100] + ' and ' + twoNum(substr.slice(1));
+    }
+  }
+
+  function screenclear() {
+    isEnglish = false;
+    isNumber = false;
+    document.getElementById("numArea").innerHTML = '';
+    document.getElementById("engArea").innerHTML = '';
+  }
+
+  var prompt = function(message, style, time) {
+    style = (style === undefined) ? 'alert-success' : style;
+    time = (time === undefined) ? 1200 : time;
+    $('<div id="promptModal">')
+        .appendTo('body')
+        .addClass('alert ' + style)
+        .css({
+            "display": "block",
+            "z-index": 99999,
+            "left": ($(document.body).outerWidth(true) - 120) / 2,
+            "top": ($(window).height() - 45) / 2,
+            "position": "absolute",
+            "padding": "20px",
+            "border-radius": "5px",
+            "color": "#fff",
+            "background-color": "#009900",
+            "opacity": 0.6,
+            "font-size": "20px",
+            "text-align": "center"
+        })
+        .html(message)
+        .show()
+        .delay(time)
+        .fadeOut(10, function () {
+            $('#promptModal').remove();
+        });
+  };
+
+  document.getElementById("numArea").innerHTML = isNumber?(isPart ? coreContext.splitNum : coreContext.randNum):'';
+  document.getElementById("engArea").innerHTML = isEnglish?coreContext.strNum:'';
+
+  document.getElementById("numArea").onclick = function () {
+    changeNumShow();
+    document.getElementById("numArea").innerHTML = isNumber?(isPart ? coreContext.splitNum : coreContext.randNum):'';
+  };
+
+  document.getElementById("createNumber").onclick = function () {
+    initContext();
+    screenclear();
+    coreContext.randNum = createNum();
+    coreContext.splitNum = num2Part(coreContext.randNum);
+    coreContext.strNum = num2En(coreContext.randNum);
+    // window.alert('生成成功');
+    changeBtnStatus("UNDEFINED");
+    ttsWS?.close();
+    audioPlayer.reset();
+    prompt('生成成功', 'alert-success', 500);
+  }
+  
+  document.getElementById("clear").onclick = function () {
+    screenclear();
+    changeBtnStatus("STOP");
+    changeBtnStatus("UNDEFINED");
+    prompt('清空成功', 'alert-success', 500);
+  }
+
+  document.getElementById("help").onclick = function () {
+    confirm("帮助:\n1. 点击生成数字，生成一个随机的数字，!!点击数字区域内的数字可以切换显示形式辅助读数!!。\n2. 点击语音，可以进行语音播放。\n3. 点击清屏，可以清空数字和英文。\n4. 在左侧第三栏可调节发音参数。\n");
+  }
+  
+  document.getElementById("showNumber").onclick = function () {
+    isNumber = true;
+    document.getElementById("numArea").innerHTML = isNumber?(isPart ? coreContext.splitNum : coreContext.randNum):'';
+  }
+  document.getElementById("showEnglish").onclick = function () {
+    isEnglish = true;
+    document.getElementById("engArea").innerHTML = isEnglish?coreContext.strNum:'';
+  }
+
+
+  audioPlayer.onPlay = () => {
+    changeBtnStatus("PLAY");
+  };
+  audioPlayer.onStop = (audioDatas) => {
+    console.log(audioDatas);
+    btnStatus === "PLAY" && changeBtnStatus("STOP");
+  };
 
   document.getElementById("speed").onchange =
     document.getElementById("volume").onchange =
